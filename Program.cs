65b371f5705ffe4e7d488ca7a4e0aaff49588615
huynh-kiet <anhkiet9913@gmail.com>
2024-11-13 15:41:33 +0700

    Use Entity Framework and create connection SQLite DB

--- Program.cs
+++ Program.cs
@@ -7,13 +7,20 @@ using Microsoft.EntityFrameworkCore;
 
 // builder => Dòng này tạo một WebApplicationBuilder, đối tượng này chịu trách nhiệm cấu hình tất cả các dịch vụ cần thiết cho ứng dụng của bạn.
 var builder = WebApplication.CreateBuilder(args);
+// Thực hiện kết nối database 
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 
 // Thêm cấu hình dịch vụ CORS 
 builder.Services.AddCors(options => {});
 
 // Sử dụng và cấu hình dịch vụ swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+// Chúng ta sẽ cmt lại phần sử dụng Inmemory để trực tiếp đưa vào SQLite cho dễ demo.
+// builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+
+// kết nối SQL DB 
+builder.Services.AddSqlite<PizzaDb>(connectionString);
+
 builder.Services.AddSwaggerGen(c =>
 {
   c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
@@ -33,12 +40,43 @@ app.UseCors("some unique string");
 //app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
 
 // Thực hiện tạo các route cho Minimal APIs theo EF
+
+// GET listing
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+
+// GET item by id
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+
+// POST Create item
 app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
 {
     await db.Pizzas.AddAsync(pizza);
     await db.SaveChangesAsync();
     return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+
+// PUT Update item by id
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
+{
+      var pizza = await db.Pizzas.FindAsync(id);
+      if (pizza is null) return Results.NotFound();
+      pizza.Name = updatepizza.Name;
+      pizza.Description = updatepizza.Description;
+      await db.SaveChangesAsync();
+      return Results.NoContent();
+});
+
+// DELETE Delete item by id
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+{
+   var pizza = await db.Pizzas.FindAsync(id);
+   if (pizza is null)
+   {
+      return Results.NotFound();
+   }
+   db.Pizzas.Remove(pizza);
+   await db.SaveChangesAsync();
+   return Results.Ok();
 });
 
 // Thực hiện thêm swagger vào add và swagger UI 
